0fc6ef2e7d5fe10a2d16065fedc9b4be78a1ce89
dorkbrain <dpcox95@gmail.com>
2022-08-15 19:06:59 -0400

    deleted:    etc/remux/deckcam1.conf 	new file:   etc/remux/deckcam1.json 	renamed:    etc/remux/index.html -> etc/remux/html/index.html 	deleted:    etc/remux/porchcam1.conf 	new file:   etc/remux/porchcam1.json 	modified:   etc/systemd/system/deckcam1.service 	modified:   etc/systemd/system/porchcam1.service 	modified:   usr/local/bin/startstream.sh

--- usr/local/bin/startstream.sh
+++ usr/local/bin/startstream.sh
@@ -4,21 +4,39 @@ if [[ -z "$1" ]]; then
   exit 1
 fi
 
-. "$1"
+# Import camera config
+cam_name="$( jq -r '.cam_name' <"$1")"
+cam_user="$( jq -r '.cam_user' <"$1")"
+cam_pass="$( jq -r '.cam_pass' <"$1")"
+cam_host="$( jq -r '.cam_host' <"$1")"
+cam_port="$( jq -r '.cam_port' <"$1")"
+cam_path="$( jq -r '.cam_path' <"$1")"
+source_html="$( jq -r '.source_html' <"$1")"
+web_path="$( jq -r '.web_path' <"$1")"
+ffoptions="$( jq -r '.ffoptions | join(" ")' <"$1" )"
 
+# Construct RTSP URL and ffmpeg command line
 rtspurl="rtsp://${cam_user}:${cam_pass}@${cam_host}${cam_path}"
-ffcmd="ffmpeg -i ${rtspurl} ${ffoptions[@]} ${web_path}/${cam_name}.m3u8"
+ffcmd="ffmpeg -i ${rtspurl} ${ffoptions} ${web_path}/${cam_name}.m3u8"
 
+# Sleep 10 seconds to allow system to settle down during boot or service restart
+echo "In 10 seconds will execute:"
+echo "  ${ffcmd}"
 sleep 10
 
-if [[ ! -f ${web_path}/index.html ]]; then
-  mkdir -p "${web_path}"
-  ln -s /etc/remux/index.html "${web_path}"/index.html
+# Make sure html root path exists
+if [[ ! -d ${web_path} ]]; then
+  mkdir -p ${web_path}
 fi
 
-cd "${web_path}"
-echo "${ffcmd}"
+# (re-)link any source files to the RAM disk
+for h in ${source_html}/*; do
+  ln -fs $h ${web_path}/$(basename $h)
+done
 
+cd "${web_path}"
+
+# Run ffmpeg in an endless loop to reconnect after any failures
 while :; do
   ${ffcmd}
   sleep 3
